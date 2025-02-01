57879c0a4906a0cf96229c5892a661e2271d1bef
Khaled Mahfoz <khaledmahfoz@users.noreply.github.com>
2025-02-01 22:32:19 +0200

    feat: adding project setup

---
+++ .eslintrc.js
@@ -0,0 +1,53 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin', 'import'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.js', '*jest.config*'],
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': [
+      'error',
+      {
+        allowExpressions: true,
+        allowTypedFunctionExpressions: true,
+      },
+    ],
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'no-console': ['error', { allow: ['warn', 'error'] }],
+    'import/order': [
+      'error',
+      {
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
+    'max-lines': [
+      'error',
+      { max: 300, skipBlankLines: true, skipComments: true },
+    ],
+  },
+  overrides: [
+    {
+      files: ['*.spec.ts', '*.spec.js'],
+      rules: {
+        'max-lines': 'off',
+      },
+    },
+  ],
+};
