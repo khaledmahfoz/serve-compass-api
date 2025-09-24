b7a3ba3926f3cf38f1f9638393897b9f9e4d0ca7
Khaled Mahfoz <khaledmahfoz@users.noreply.github.com>
2025-09-24 04:43:51 +0300

    feat: adding winston logger; configure bullMQ retry options

--- src/lib/providers/redis-options.ts
+++ src/lib/providers/redis-options.ts
@@ -1,4 +1,5 @@
 import { CacheModuleAsyncOptions } from '@nestjs/cache-manager';
+import { InternalServerErrorException, Logger } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { redisStore } from 'cache-manager-redis-store';
 
@@ -6,17 +7,24 @@ export const RedisOptionsProvider: CacheModuleAsyncOptions = {
   isGlobal: true,
   imports: [ConfigModule],
   useFactory: async (configService: ConfigService) => {
-    const store = await redisStore({
-      socket: {
-        host: configService.get<string>('REDIS_HOST'),
-        port: parseInt(configService.get<string>('REDIS_PORT')!),
-      },
-      password: configService.get('REDIS_PASSWORD'),
-      username: configService.get('REDIS_USERNAME'),
-    });
-    return {
-      store: () => store,
-    };
+    const logger = new Logger('RedisOptionsProvider');
+    try {
+      const store = await redisStore({
+        socket: {
+          host: configService.get<string>('REDIS_HOST'),
+          port: parseInt(configService.get<string>('REDIS_PORT')!),
+        },
+        password: configService.get('REDIS_PASSWORD'),
+        username: configService.get('REDIS_USERNAME'),
+      });
+      logger.log('Redis connection was successful');
+      return {
+        store: () => store,
+      };
+    } catch (error) {
+      logger.error(`Redis connection error: ${error}`);
+      throw new InternalServerErrorException('Redis connection error');
+    }
   },
   inject: [ConfigService],
 };
